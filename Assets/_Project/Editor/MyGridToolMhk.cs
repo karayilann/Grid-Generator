dcc86238676c8b79af79fe191ac94a4c3f9abfdc
Muhammet Karayilan <muhammethamitkarayilan@gmail.com>
2024-04-08 00:47:48 +0300

    The trial code snippets have been deleted

--- Assets/_Project/Editor/MyGridToolMhk.cs
+++ Assets/_Project/Editor/MyGridToolMhk.cs
@@ -10,11 +10,7 @@ namespace _Project.Editor
         [MenuItem("Tools/My Grid Tool")]
         static void Init()
         {
-            Debug.Log("Muhammet Hamit Karayılan");
-            Debug.Log("https://linktr.ee/karayilann");
-
-            // Prefab instantiate işlemi
-            GameObject prefab = Resources.Load<GameObject>("MyGridToolMhk"); // Prefab ismi ile değiştirilmeli
+            GameObject prefab = Resources.Load<GameObject>("MyGridToolMhk"); 
             if (prefab != null)
             {
                 Instantiate(prefab, Vector3.zero, Quaternion.identity);
@@ -27,13 +23,6 @@ namespace _Project.Editor
 
         public override void OnInspectorGUI()
         {
-            if (GUILayout.Button("Go To Developer Page"))
-            {
-                Application.OpenURL("https://linktr.ee/karayilann");
-            }
-
-            GUILayout.Space(20);
-
             base.OnInspectorGUI();
 
             GridGenerator _gridGenerator = (GridGenerator)target;
